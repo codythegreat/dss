72de18b882e6f21fb068b3c3bbc4bc39af984955
codythegreat <maxiecody@gmail.com>
2019-10-26 11:08:57 -0500

    added slides.h inport

--- include/parser.h
+++ include/parser.h
@@ -1,13 +1,14 @@
 #if !defined( PARSER_H )
 #define PARSER_H
+#include "slides.h"
 
-typedef struct
-{
-    char content[5000];
-    int number;
-    int x, y, r, g, b;
-} Slide;
+//typedef struct
+//{
+    //char content[5000];
+    //int number;
+    //int x, y, r, g, b;
+//} Slide;
 
-Slide* parseTXT(FILE *inFile, int* slideCounter, char *presTitle);
+slide* parseTXT(FILE *inFile, int* slideCounter, char *presTitle);
 
 #endif
