61ab05cee0827d2ff5a0078a2676362540860510
codythegreat <maxiecody@gmail.com>
2019-10-30 20:29:01 +0000

    todo for better arg implementation, add bookmark command

--- include/parsecommand.h
+++ include/parsecommand.h
@@ -3,13 +3,14 @@
 
 typedef struct command {
     short cmd;
+    // todo: better way to do the below? array or string pointers?
     char arg[10][256];
 } command;
 // is this the best way to handle commands?
 //           0 - bad input
 // q/Q       1 - close program
 // open      2 - open a new file
-// tabopen   3 - open a new file on another tab
+// tabopen   3 - bookmark current slide
 // blist     4 - bookmark list
 // bclear    5 - bookmark clear
 // n         7 - jump to slide
