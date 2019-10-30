6eb6196d50d8eaa6be876a1593c518de8984e681
codythegreat <maxiecody@gmail.com>
2019-10-30 18:05:13 +0000

    general code cleanup and commentation

--- src/parsecommand.c
+++ src/parsecommand.c
@@ -4,15 +4,21 @@
 #include <string.h>
 #include "parsecommand.h"
 
+// holds user input while in command mode
 char userInput[1000];
+// y value of bottom of screen
 int bottomLine;
 
 command* parseCommand()
 {
-    // todo: add more if statements for commands
+    // create a new command struct (to be returned)
     command* comm = malloc(sizeof(struct command));
+
+    // *p holds current arg sep by spaces
     char *p;
     p = strtok(userInput, " ");
+
+    // iterate over each argument and add to comm->arg array
     int i = 0;
     while (p!=NULL)
     {
@@ -22,6 +28,8 @@ command* parseCommand()
         i++;
     }
     
+    // todo: add more if statements for commands
+    // todo: can this be done better?
     if (strcmp(comm->arg[0], "q")==0) 
     {
          comm->cmd = 1;
@@ -41,21 +49,27 @@ command* parseCommand()
     {
          comm->cmd = 0;
     }
+
     return comm;
 }
 
 
 command* commandLoop(int *y)
 {
+    // move to bottom of screen, clear line and print :
     bottomLine = *y-1;
     move(bottomLine, 1);
     // clear to end of line for printing
     clrtoeol();
     printw(":");
 
-    char keyPress;
-    int currentCharacter = 0;
+    // bool becomes false after enter is pressed
     bool typing = true;
+
+    // tracks character for keyPress to userInput
+    int currentCharacter = 0;
+
+    char keyPress;
     while (typing)
     {
         keyPress = getch();
@@ -66,25 +80,27 @@ command* commandLoop(int *y)
             break;
         case 27:    // esc
             break;
-	    case 127:   // back space  todo: test key input on mult systems
+        case 127:   // back space  todo: test key input on mult systems
             if (currentCharacter != 0) {
                 // move back and delete character in place
                 mvdelch(bottomLine, 1+currentCharacter);
                 currentCharacter--;
 	        }
-		    break;
+            break;
         default:
-	        // print the character to the screen
+            // print the character to the screen
             addch(keyPress);
-		    // add the character to our userIn buffer
-	        userInput[currentCharacter] = keyPress;
-	        userInput[currentCharacter+1] = '\0';
-	        currentCharacter++;
-		    break;
+            // add the character to our userIn buffer
+            userInput[currentCharacter] = keyPress;
+            userInput[currentCharacter+1] = '\0';
+            currentCharacter++;
+    	    break;
 	}
     }
+
     // hide cursor
     curs_set(0);
+
     // parse input into a command struct and return it
     return parseCommand();
 }
