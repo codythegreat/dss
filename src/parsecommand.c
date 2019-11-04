a9805e4a696a67defb2a200bb35b8a27ffa36d95
codythegreat <maxiecody@gmail.com>
2019-11-04 15:51:51 +0000

    remove commandLoop, parseCommand converts string arg into command struct

--- src/parsecommand.c
+++ src/parsecommand.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <curses.h>
 #include <string.h>
 #include "parsecommand.h"
 
-// holds user input while in command mode
-char userInput[1000];
-// y value of bottom of screen
-int bottomLine;
-
-command* parseCommand()
+command* parseCommand(char commandInput[1000])
 {
     // create a new command struct (to be returned)
     command* comm = malloc(sizeof(struct command));
 
     // *p holds current arg sep by spaces
     char *p;
-    p = strtok(userInput, " ");
+    p = strtok(commandInput, " ");
 
     // iterate over each argument and add to comm->arg array
     int i = 0;
@@ -54,59 +48,4 @@ command* parseCommand()
     }
 
     return comm;
-}
-
-
-command* commandLoop(int *y)
-{
-    // move to bottom of screen, clear line and print :
-    bottomLine = *y-1;
-    move(bottomLine, 1);
-    // clear to end of line for printing
-    clrtoeol();
-    printw(":");
-    
-    // enable cursor
-    curs_set(1);
-
-    // bool becomes false after enter is pressed
-    bool typing = true;
-
-    // tracks character for keyPress to userInput
-    int currentCharacter = 0;
-
-    char keyPress;
-    while (typing)
-    {
-        keyPress = getch();
-	switch(keyPress)
-	{
-        case '\n':  // enter
-            typing = false;
-            break;
-        case 27:    // esc
-            break;
-        case 127:   // back space  todo: test key input on mult systems
-            if (currentCharacter != 0) {
-                // move back and delete character in place
-                mvdelch(bottomLine, 1+currentCharacter);
-                currentCharacter--;
-	        }
-            break;
-        default:
-            // print the character to the screen
-            addch(keyPress);
-            // add the character to our userIn buffer
-            userInput[currentCharacter] = keyPress;
-            userInput[currentCharacter+1] = '\0';
-            currentCharacter++;
-    	    break;
-	}
-    }
-
-    // hide cursor
-    curs_set(0);
-
-    // parse input into a command struct and return it
-    return parseCommand();
 }
