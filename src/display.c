f26d88e256b14579b39334df43fea847d03e8d55
codythegreat <maxiecody@gmail.com>
2019-11-08 16:21:32 +0000

    change how colors work: t changes forground while T changes background

--- src/display.c
+++ src/display.c
@@ -33,6 +33,98 @@ int BACKWARD = 0; // N
 int bookmarkReg; // holds key to assign bookmark to
 short bookmarks[128] = {-1}; // each index represents an ascii key
 
+const int FGBGCOLORPAIRS[56][2] = {
+    {1, 0},
+    {2, 0},
+    {3, 0},
+    {4, 0},
+    {5, 0},
+    {6, 0},
+    {7, 0},
+    {0, 1},
+    {2, 1},
+    {3, 1},
+    {4, 1},
+    {5, 1},
+    {6, 1},
+    {7, 1},
+    {0, 2},
+    {1, 2},
+    {3, 2},
+    {4, 2},
+    {5, 2},
+    {6, 2},
+    {7, 2},
+    {0, 3},
+    {1, 3},
+    {2, 3},
+    {4, 3},
+    {5, 3},
+    {6, 3},
+    {7, 3},
+    {0, 4},
+    {1, 4},
+    {2, 4},
+    {3, 4},
+    {5, 4},
+    {6, 4},
+    {7, 4},
+    {0, 5},
+    {1, 5},
+    {2, 5},
+    {3, 5},
+    {4, 5},
+    {6, 5},
+    {7, 5},
+    {0, 6},
+    {1, 6},
+    {2, 6},
+    {3, 6},
+    {4, 6},
+    {5, 6},
+    {7, 6},
+    {0, 7},
+    {1, 7},
+    {2, 7},
+    {3, 7},
+    {4, 7},
+    {5, 7},
+    {6, 7}
+};
+
+// keep the same background color, change foreground
+void incrememntFGColor() {
+        int currentBG = FGBGCOLORPAIRS[curColor-1][1];
+        do {
+            curColor++;
+            if (curColor == 57) {
+                curColor = 1;
+            }
+        } while (FGBGCOLORPAIRS[curColor-1][1] != currentBG);
+        // sets the screen to the new color pair 
+        wbkgd(stdscr, COLOR_PAIR(curColor));
+}
+
+// keep the same foreground color, change background
+void incrememntBGColor() {
+        int currentFG = FGBGCOLORPAIRS[curColor-1][0];
+        do {
+            curColor++;
+            if (curColor == 57) {
+                curColor = 1;
+            }
+        } while (FGBGCOLORPAIRS[curColor-1][0] != currentFG);
+        // sets the screen to the new color pair 
+        wbkgd(stdscr, COLOR_PAIR(curColor));
+}
+
+void initColors() {
+    int i;
+    for (i=0;i<56;i++) {
+        init_pair(i+1, FGBGCOLORPAIRS[i][0], FGBGCOLORPAIRS[i][1]);
+    }
+}
+
 void initDisplay()
 {
     // initialize ncurses
@@ -40,11 +132,7 @@ void initDisplay()
     // if term supports color, enable
     if (has_colors()) {
         start_color();
-        init_pair(1, COLOR_WHITE, COLOR_BLACK);
-        init_pair(2, COLOR_MAGENTA, COLOR_WHITE);
-        init_pair(3, COLOR_WHITE, COLOR_BLUE);
-        init_pair(4, COLOR_BLACK, COLOR_WHITE);
-        init_pair(5, COLOR_BLACK, COLOR_CYAN);
+        initColors();
     }
     // disable line buffering
     cbreak();
@@ -384,12 +472,12 @@ slide* handleKeyPress(slide *curSlide)
             break;
         case 't': // change color if term has support
             if(has_colors()) {
-                curColor++;
-                if (curColor == 6) {
-                    curColor = 1;
-                }
-                // sets the screen to the new color pair 
-                wbkgd(stdscr, COLOR_PAIR(curColor));
+                incrememntFGColor();
+            }
+            break;
+        case 'T': // next background color
+            if(has_colors()) {
+                incrememntBGColor();
             }
             break;
         case 'b': // set a bookmark on cur slide
